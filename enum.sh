f6e419e118000d0a56e446bb37bf39812c29a33d
Philip <hive@philip-griese.de>
2018-05-04 12:38:03 +0200

    Add files via upload

--- enum.sh
+++ enum.sh
@@ -5,10 +5,10 @@ TMPFILE=~/Desktop/AppEnumeration.txt
 uname -a  >> $TMPFILE
 pwd >> $TMPFILE
 find /Applications/ -name "*.app"  >> $TMPFILE
-find ~/Applications/ -name "*.app" >> $TMPFILE
-find ~/Library/Application\ Support/Google/Chrome/Default/Extensions/ -name manifest.json -exec grep "\"name\"" \{\} \; >> $TMPFILE
-find /Users/philip/Library/Application\ Support/Firefox/Profiles/*default/extensions -depth 1 >> $TMPFILE
-find ~/Library/Safari/Extensions/*safariextz >> $TMPFILE
+find ~/Applications/ -name "*.app"  2> /dev/null >> $TMPFILE
+find ~/Library/Application\ Support/Google/Chrome/Default/Extensions/ -name manifest.json -exec grep "\"name\"" \{\} \;  2> /dev/null >> $TMPFILE
+find /Users/philip/Library/Application\ Support/Firefox/Profiles/*default/extensions -depth 1  2> /dev/null >> $TMPFILE
+find ~/Library/Safari/Extensions/*safariextz  2> /dev/null >> $TMPFILE
 
-
-
+echo "FINISHED"
+echo "Application enumeration finished. There is a file called AppEnumeration.txt on your desktop now, please mail it to Phil"
